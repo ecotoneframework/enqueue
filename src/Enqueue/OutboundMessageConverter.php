7e1800de8327a963ca184280e2f6b1de29500d26
Dariusz Gafka <dgafka.mail@gmail.com>
2020-08-19 13:00:29 +0200

    unset ack location

--- src/Enqueue/OutboundMessageConverter.php
+++ src/Enqueue/OutboundMessageConverter.php
@@ -96,7 +96,7 @@ class OutboundMessageConverter
         unset($applicationHeaders[MessageHeaders::DELIVERY_DELAY]);
         unset($applicationHeaders[MessageHeaders::TIME_TO_LIVE]);
         unset($applicationHeaders[MessageHeaders::CONTENT_TYPE]);
-        unset($applicationHeaders[MessageHeaders::CONSUMER_ACK_HEADER_LOCATION]);
+        unset($applicationHeaders[$applicationHeaders[MessageHeaders::CONSUMER_ACK_HEADER_LOCATION]], $applicationHeaders[MessageHeaders::CONSUMER_ACK_HEADER_LOCATION]);
         unset($applicationHeaders[MessageHeaders::CONSUMER_ENDPOINT_ID]);
         unset($applicationHeaders[MessageHeaders::POLLED_CHANNEL_NAME]);
         unset($applicationHeaders[MessageHeaders::POLLED_CHANNEL]);
